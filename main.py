4df75c2106794090b860c37a422023e948b73f9e
liamgines <109954426+liamgines@users.noreply.github.com>
2026-04-02 02:19:50 -0700

    List initial repository files in system message

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from llama_cpp import Llama
 import os
 import subprocess
+import mimetypes
 
 # CHAT_FORMAT must be set according to the model
 CHAT_FORMAT = "chatml"
 MODEL_PATH = "model.gguf"
-SYSTEM_CONTENT = "You are an assistant who perfectly summarizes information and answers questions." 
+DEFAULT_SYSTEM_CONTENT = "You are an assistant who perfectly summarizes information and answers questions." 
 CURRENT_WORKING_DIRECTORY = os.getcwd()
 
 def chat_completion_get_contents(chat_completion):
@@ -49,13 +50,35 @@ def path_is_repository_message(path):
         return "(git repository)"
     return "(not a git repository)"
 
-def main():
-    git_repository_path = CURRENT_WORKING_DIRECTORY
+def path_get_repository_files(path):
+    git_repository_files = []
+    if (path_is_repository(path)):
+        git_repository_files = subprocess.run(["git", "ls-tree", "--full-tree", "-r", "--name-only", "HEAD"], capture_output=True, text=True).stdout.split()
 
-    llm = Llama(model_path=MODEL_PATH, chat_format=CHAT_FORMAT, verbose=False)
+    return git_repository_files
+
+def main():
+    system_content = DEFAULT_SYSTEM_CONTENT
+    git_repository_path = CURRENT_WORKING_DIRECTORY
+    git_repository_files = path_get_repository_files(git_repository_path)
+    if git_repository_files:
+        system_content += "\nYou may use the following files to help you answer the questions if necessary:\n\n"
+        for file_name in git_repository_files:
+            absolute_file_path = os.path.join(git_repository_path, file_name)
+            mime_type = mimetypes.guess_type(absolute_file_path)[0]
+            if not mime_type or not mime_type.startswith("text/"):
+                continue
+
+            system_content += file_name + ":\n"
+            with open(absolute_file_path, "r") as file:
+                file_data = file.read().rstrip()
+                system_content += file_data + "\n\n"
+
+    # if n_ctx is 0, it is determined from the model
+    llm = Llama(model_path=MODEL_PATH, chat_format=CHAT_FORMAT, n_ctx=0, verbose=False)
 
     messages = []
-    system_message = { "role": "system", "content": SYSTEM_CONTENT }
+    system_message = { "role": "system", "content": system_content }
     messages.append(system_message)
 
     while True:
